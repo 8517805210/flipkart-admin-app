41a0649e422a8ab80c0ef2a1930f830fc129943a
Mohit <mohitkumar11742@gmail.com>
2021-08-10 11:43:12 +0530

    udated baseApi and api in urlconfig.js

--- urlConfig.js
+++ urlConfig.js
@@ -1,4 +1,6 @@
-export const api='http://localhost:2000/api';
+const baseApi=location.hostname==='localhost'?"http://localhost:2000":" https://flipkart-back-end-app.herokuapp.com"
+
+export const api=`${baseApi}/api`;
 export const generatePublicUrl=(fileName)=>{
-    return  `http://localhost:2000/public/${fileName}`
+    return  `${baseApi}/${fileName}`
 }
